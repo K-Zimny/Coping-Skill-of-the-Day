d92ebe8f3bebfe7fddc63a2dc94d23850025f3a7
kzim <k.zimny1994@gmail.com>
2022-06-29 23:58:45 -0500

    Initial commit

---
+++ notes.md
@@ -0,0 +1,1 @@
+Make a whole category for each skill, like a database you can just look through
---
+++ script.js
@@ -0,0 +1,37 @@
+const copingSkills = [
+  "Deep Breathing",
+  "Listening to Music",
+  "Body Scan",
+  "Moment to Pause",
+  "Be kind to yourself",
+  "Exercise",
+  "Connect with Others",
+  "Acknowledge your positives",
+  "Mindfulness/Meditation",
+  "Practice Gratitude",
+  "Visualization",
+  "Progressive Muscle Relaxation",
+  "Do what needs to be done",
+  "Practice Wise Mind",
+  "Notice what energizes you, what you are grateful for, and what's not working and why",
+  "Practice self love",
+  "Laughter",
+  "Build self esteem",
+  "Journal",
+  "Get outside",
+];
+const copingSkillParagraphElement = document.querySelector(".coping-skill");
+const newButton = document.querySelector("button");
+let copingSkillIndex = Math.floor(Math.random() * copingSkills.length);
+
+copingSkillParagraphElement.innerHTML = copingSkills[copingSkillIndex];
+copingSkills.splice(copingSkillIndex, 1);
+
+newButton.addEventListener("click", function () {
+  copingSkillIndex = Math.floor(Math.random() * copingSkills.length);
+  copingSkillParagraphElement.innerHTML = copingSkills[copingSkillIndex];
+  if (copingSkills.length === 0) {
+    copingSkillParagraphElement.innerHTML = "No more Skills. :(";
+  }
+  copingSkills.splice(copingSkillIndex, 1);
+});
