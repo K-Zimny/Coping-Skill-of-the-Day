faffad46d2454588fc83a3edcf3605b8fa0faeaa
kzim <k.zimny1994@gmail.com>
2022-08-15 19:11:43 -0500

    Added positive qualities record base code

--- input.js
+++ input.js
@@ -1,60 +1,27 @@
-const textField = document.querySelector(".input-field");
-const submit = document.querySelector(".submit");
+// local storage
+const textFieldOne = document.querySelector(".input-field-one");
+const textFieldTwo = document.querySelector(".input-field-two");
+const textFieldThree = document.querySelector(".input-field-three");
 
-submit.addEventListener("click", function () {
-  localStorage.input = textField.value;
-
-  console.log(localStorage.input);
+textFieldOne.addEventListener("change", function () {
+  localStorage.inputOne = textFieldOne.value;
+});
+textFieldTwo.addEventListener("change", function () {
+  localStorage.inputTwo = textFieldTwo.value;
+});
+textFieldThree.addEventListener("change", function () {
+  localStorage.inputThree = textFieldThree.value;
 });
 
-textField.value = localStorage.input;
+textFieldOne.value = localStorage.inputOne;
+textFieldTwo.value = localStorage.inputTwo;
+textFieldThree.value = localStorage.inputThree;
 
-// const textField = document.querySelector(".input-field");
-// const submit = document.querySelector(".submit");
-// let textFieldValue = textField.value;
-// let textFieldString = textFieldValue;
+// --------------------------------------------------
+// Reset Local Storage at midnight
+let today = new Date();
+var time = today.getHours();
 
-// function setCookie(cname, cvalue, exdays) {
-//   const d = new Date();
-//   d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
-//   let expires = "expires=" + d.toUTCString();
-//   document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-// }
-
-// function getCookie(cname) {
-//   let name = cname + "=";
-//   let ca = document.cookie.split(";");
-//   for (let i = 0; i < ca.length; i++) {
-//     let c = ca[i];
-//     while (c.charAt(0) == " ") {
-//       c = c.substring(1);
-//     }
-//     if (c.indexOf(name) == 0) {
-//       return c.substring(name.length, c.length);
-//     }
-//   }
-//   return "";
-// }
-
-// function checkCookie() {
-//   let user = getCookie("username");
-//   if (user != "") {
-//     alert("Welcome again " + user);
-//   } else {
-//     user = prompt("Please enter your name:", "");
-//     if (user != "" && user != null) {
-//       setCookie("username", user, 365);
-//     }
-//   }
-// }
-// console.log(textField);
-// // setCookie("input", textFieldString, 1);
-// //   getCookie("input");
-// //   checkCookie();
-// // setCookie("input", "test", 2);
-// // console.log(textFieldString);
-
-// submit.addEventListener("click", function () {
-//   setCookie("input", textField, 1);
-//   console.log(textFieldString);
-// });
+if (time == 24) {
+  localStorage.input = "What is a positive quality";
+}
